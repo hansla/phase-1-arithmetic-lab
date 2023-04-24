ac5778986065c1dee7d239d41882ddb50b921ef9
hansla <hansla96@gmail.com>
2023-04-24 07:03:24 -0700

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1=31 
+let num2= 2
+let num3=16
+let num4 = 4
+function multiply (num1 , num2) {
+   num1*num2; 
+};
+var multiply = 62
 
-// Write your code here
+var random = Math.floor(Math.random() * 10) + 1;
+
+function mod (num3 , num4){
+   num3/num4;
+}
+var mod = 4;
+
+var max = Math.max(1, 2, 3, 4, 8, 20);
